28c7f17cfa387f363a716fe1e7ed2d06fc0f94c8
etyumemntcev <etyumentacve@gmail.com>
2023-12-18 11:19:38 +0600

    Описан класс CollisionDetector, чтобы можно было написать тесты. Определены два теста - когда коллизия обнаружена и когда коллизии нет.

---
+++ Spacebattle/Spacebattle/CollisionDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacebattle
+{
+    public class CollisionDetector<T>
+    {
+        public void Add(IEnumerable<T> sample)
+        {
+
+        }
+
+        public void Detect(IEnumerable<T> pattern)
+        {
+
+        }
+    }
+}
---
+++ Spacebattle/SpacebattleTests/CollisionDetectorTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spacebattle
+{
+    public class CollisionDetectorTests
+    {
+        [Fact]
+        public void CollissionDetectorSholudFindKnownPatterns()
+        {
+
+            var detector = new CollisionDetector<int>();
+
+            bool wasDetected = false;
+            Action action = () => { wasDetected = true; };
+            // сюда добавить подписку на событие - коллизия обнаружена
+
+            detector.Add(new int[] { 2, 7, 8, -3 });
+            detector.Add(new int[] { 2, 7, 8, 2 });
+            detector.Add(new int[] { 2, 7, 8, 15 });
+
+            detector.Detect(new int[] { 2, 7, 8, 2 });
+
+            Assert.True(wasDetected);
+        }
+
+        [Fact]
+        public void CollissionDetectorSholudNotFindUnknownPatterns()
+        {
+
+            var detector = new CollisionDetector<int>();
+
+            bool wasDetected = false;
+            Action action = () => { wasDetected = true; };
+            // сюда добавить подписку на событие - коллизия обнаружена
+
+            detector.Add(new int[] { 2, 7, 8, -3 });
+            detector.Add(new int[] { 2, 7, 8, 15 });
+
+            detector.Detect(new int[] { 2, 7, 8, 2 });
+
+            Assert.False(wasDetected);
+        }
+    }
+}
