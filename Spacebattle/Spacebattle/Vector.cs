939505271de338ca56c7e7f3840c496cb98f3918
etyumemntcev <etyumentacve@gmail.com>
2023-12-18 10:54:22 +0600

    Добавлены заглушки в проект, чтобы проект собирался.

--- Spacebattle/Spacebattle/Vector.cs
+++ Spacebattle/Spacebattle/Vector.cs
@@ -21,37 +21,44 @@ namespace Spacebattle
             get
             {
                 //Задача 2
+                return 0; // Это заглушка, чтобы компилировался код
             }
         }
 
         public static implicit operator Vector<T>(T[] a)
         {
             //Задача 3
+            return null; // Это заглушка, чтобы компилировался код
         }
 
         IEnumerator<T> IEnumerable<T>.GetEnumerator()
         {
             //Задача 4
+            return null; // Это заглушка, чтобы компилировался код
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             //Задача 4
+            return null; // Это заглушка, чтобы компилировался код
         }
 
         public override string ToString()
         {
             //Задача 5
+            return string.Empty; // Это заглушка, чтобы компилировался код
         }
 
         public static T[] Parse(string value, Func<string, T> parse)
         {
             //Задача 6
+            return new T[0]; // Это заглушка, чтобы компилировался код
         }
 
         public static Vector<T> operator +(Vector<T> a, Vector<T> b)
         {
             //Задача 7
+            return null; // Это заглушка, чтобы компилировался код
         }
 
     }
