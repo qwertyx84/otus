cbe564979c8f2d3734790a81aeac5aae1e122a04
qwertyx84 <qwertyx84@mail.ru>
2024-02-26 23:01:55 +0400

    HW3 Nemtyrev Oleg

---
+++ Spacebattle/Spacebattle/Extensions/SpaceshipExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Spacebattle.Interfaces;
+
+namespace Spacebattle.Extensions
+{
+    public static class SpaceshipExtensions
+    {
+        public static IEnumerable<ISpaceShip> GetSpaceshipsByPlayerId(this IEnumerable<ISpaceShip> spaceships, int playerId)
+        {
+            return spaceships.Where(s => s.PlayerId == playerId);
+        }
+
+        public static IGameObject? GetObjectById(this IEnumerable<IGameObject> gameObjects, int objectId)
+        {
+            return gameObjects.FirstOrDefault(obj => obj.ObjectId == objectId);
+        }
+
+        public static IEnumerable<IGameObject> GetMovingObjects(this IEnumerable<ISpaceShip> spaceships)
+        {
+            return spaceships.Where(obj => obj.Moving);
+        }
+    }
+}
---
+++ Spacebattle/Spacebattle/Interfaces/Spacebattle.Interfaces.cs
@@ -0,0 +1,15 @@
+﻿namespace Spacebattle.Interfaces
+{
+
+    public interface IGameObject
+    {
+        public int ObjectId { get; }
+        public string ObjectName { get; }
+        bool Moving { get; }
+    }
+    public interface ISpaceShip : IGameObject
+    {
+      public int PlayerId { get; }
+      
+    }
+}
