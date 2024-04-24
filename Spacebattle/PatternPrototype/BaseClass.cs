497eb6d69ff7d41fddd269241fbaf17ef69fb093
qwertyx84 <qwertyx84@mail.ru>
2024-04-24 20:51:13 +0400

    HW#11_NemtyrevOleg

---
+++ Spacebattle/PatternPrototype/BaseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass
+{
+    // Базовый класс Shape
+    public abstract class Shape : IMyCloneable<Shape>
+    {
+        public abstract Shape Clone();
+    }
+
+}
